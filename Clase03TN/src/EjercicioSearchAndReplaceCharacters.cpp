40f482e86dc85d09b142002e4b374355ecd59b7d
SosaRaul <tecraulsosa@gmail.com>
2022-09-09 06:25:04 -0300

    fix bugs in BuscarTexto & ContieneSubTexto

--- Clase03TN/src/EjercicioSearchAndReplaceCharacters.cpp
+++ Clase03TN/src/EjercicioSearchAndReplaceCharacters.cpp
@@ -28,27 +28,55 @@ int BuscarTexto(char texto[],char textoBusqueda[]);
 
 int main() {
 
-	char texto[] = "stucabatrk";
-	char subTexto[] = "caa";
-	cout << BuscarTexto(texto,subTexto);
+	char texto[] = "hola como andas juan andas bien";
+	char textoBusqueda[] = "andas";
+	char textoReemplazo[] = "raul";
+
+	cout << "texto : " << texto  << endl;
+	cout << "textoBusqueda : " << textoBusqueda << endl;
+	cout << "textoReemplazo : " << textoReemplazo << endl;
+	Reemplazar(texto,textoBusqueda,textoReemplazo);
+	cout << "Texto luego del reemplazo : " << texto << endl;
+
 
 	return 0;
 }
-
+// Esta implementación solo reemplaza, teniendo en cuenta una única coincidencia
+// TODO : Mejorarla para que reemplace todas las coincidencias.
 void Reemplazar(char texto[], char textoBusqueda[],char textoReemplazo[]){
 
+	int indiceComienzo = BuscarTexto(texto,textoBusqueda);
+
+	unsigned int indiceTextoReemplazo = 0;
+
+	if(indiceComienzo != -1){
+		for(indiceTextoReemplazo = 0 ; indiceTextoReemplazo < strlen(textoReemplazo);indiceTextoReemplazo++){
+			texto[indiceComienzo] = textoReemplazo[indiceTextoReemplazo];
+			indiceComienzo++;
+
+		}
+	}
+	else{
+		cout << "No se encontró el texto de búsqueda!! " << endl;
+	}
 }
 bool ContieneSubTexto(char texto[] , char subTexto[]){
 
-	unsigned int indiceTexto = 0, indiceSubtexto = 0;
+	unsigned int indiceTexto = 0, indiceSubtexto = 0 , contadorCoincidencias = 0 ;
 
 	while(indiceTexto < strlen(texto) && indiceSubtexto < strlen(subTexto)){
 		if(texto[indiceTexto] == subTexto[indiceSubtexto]){
 			indiceSubtexto++;
+			contadorCoincidencias++;
+		}
+		else{
+			contadorCoincidencias = 0;
+			indiceSubtexto = 0;
 		}
 		indiceTexto++;
 	}
-	if(indiceSubtexto == strlen(subTexto)-1){
+
+	if(contadorCoincidencias == strlen(subTexto)){
 		return true;
 	}
 	else{
@@ -72,16 +100,24 @@ int BuscarCaracter(char texto[], char caracter){
 
 int BuscarTexto(char texto[],char textoBusqueda[]){
 
-	unsigned int indiceTexto = 0, indiceTextoBusqueda = 0;
+	unsigned int indiceTexto = 0, indiceTextoBusqueda = 0, contadorCoincidencias = 0;
 
 	while(indiceTexto < strlen(texto) && indiceTextoBusqueda < strlen(textoBusqueda)){
+
 		if(texto[indiceTexto] == textoBusqueda[indiceTextoBusqueda]){
 			indiceTextoBusqueda++;
-		}
-		indiceTexto++;
-	}
+			contadorCoincidencias++;
 
-	if(indiceTextoBusqueda == strlen(textoBusqueda)-1){
+		}
+		else{
+			contadorCoincidencias = 0;
+			indiceTextoBusqueda = 0;
+		}
+
+		indiceTexto++;
+
+	}
+	if(contadorCoincidencias == strlen(textoBusqueda)){
 		return indiceTexto-strlen(textoBusqueda);
 	}
 	else{
